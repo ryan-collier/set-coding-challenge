7161d26c761e21abe0c9e72a414a49ed43cda119
Ryan <ryan.mcollier@gmail.com>
2024-09-03 16:57:24 -0400

    Add new Edit Todo suite to create and edit a todo item.

--- tests/todo-app.spec.ts
+++ tests/todo-app.spec.ts
@@ -7,7 +7,8 @@ test.beforeEach(async ({ page }) => {
 
 const TODO_ITEMS = [
   'complete code challenge',
-  'ensure coverage for all items is automated'
+  'ensure coverage for all items is automated',
+  'submit code challenge'
 ];
 
 test.describe('Create New Todo', () => {
@@ -33,7 +34,43 @@ test.describe('Create New Todo', () => {
       TODO_ITEMS[0],
       TODO_ITEMS[1]
     ]);
-
+    
+    // Make sure the list now has two todo items.
+    await expect(page.getByTestId('todo-title')).toHaveText([
+      TODO_ITEMS[0],
+      TODO_ITEMS[1],
+    ]);
     await checkNumberOfTodosInLocalStorage(page, 2);
+  });
+});
+
+test.describe('Edit Todo', () => {
+  test('should be able to edit a todo item', async ({ page }) => {
+
+    // Create a new todo locator
+    const newTodo = page.getByPlaceholder('What needs to be done?');
+
+    // Create 1st todo.
+    await newTodo.fill(TODO_ITEMS[0]);
+    await newTodo.press('Enter');
+
+    // Verify the first todo item is created
+    await expect(page.getByTestId('todo-title')).toHaveText([TODO_ITEMS[0]]);
+
+    // Double-click the first todo item to edit it
+    const todoItem = page.getByTestId('todo-title').nth(0);
+    await todoItem.dblclick();
+
+    // Edit the todo item and press Enter to save changes
+    const todoEditInput = page.locator('.editing .edit');
+    const UPDATED_TODO = 'updated code challenge';
+    await todoEditInput.fill(UPDATED_TODO);
+    await todoEditInput.press('Enter');
+
+    // Verify the todo item is updated with new changes
+    await expect(page.getByTestId('todo-title')).toHaveText([UPDATED_TODO]);
+
+    // Verify the number of todos in local storage is still 1
+    await checkNumberOfTodosInLocalStorage(page, 1);
   });
 });
